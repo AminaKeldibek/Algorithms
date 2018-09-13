b4d8afb7c2c27ef6a335181679f27b8822ed9261
Amina Keldibek <amina.keldibek@gmail.com>
2018-09-13 22:29:40 +0600

    add

--- leetcode/Trees/detect_cycle_undir.py
+++ leetcode/Trees/detect_cycle_undir.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class DSU(object):
     def __init__(self, nodes):
         self.sets = [self.make_set(node) for node in nodes]
@@ -47,3 +50,36 @@ class Solution(object):
         for edge in edges:
             if dsu_solver.cycle_detected(edge[0], edge[1]):
                 return edge
+
+
+class SolutionDFS(object):
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = defaultdict(list)
+
+        def dfs(node, target):
+            if not visited[node - 1]:
+                visited[node - 1] = True
+                if node == target:
+                    return True
+                return any(dfs(n, target) for n in graph[node])
+
+        for u, v in edges:
+            visited = [False] * len(edges)
+            if u in graph.keys() and v in graph.keys() and dfs(u, v):
+                return u, v
+            graph[u].append(v)
+            graph[v].append(u)
+
+
+def main():
+    edges = [[3,4],[1,2],[2,4],[3,5],[2,5]]
+    print SolutionDFS().findRedundantConnection(edges)
+    print [1, 4]
+
+
+if __name__ == "__main__":
+    main()
